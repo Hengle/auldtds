cea908cc98e78c28394f23eb007a4bd462204818
gantzoulis <gantzoulis@hotmail.com>
2018-01-29 20:01:29 +0200

    [GA] Achievements Update - Time Survival

--- Assets/Production/Achievements/AchievementDB/AchievementCountSurvival.cs
+++ Assets/Production/Achievements/AchievementDB/AchievementCountSurvival.cs
@@ -1,16 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class AchievementCountSurvival : MonoBehaviour {
+public class AchievementCountSurvival : MonoBehaviour
+{
+    
+    public float timeToSurvive;
+    public bool isAwarded = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private GameObject achievementManager;
+    private AchievementManager amScript;
+
+    private int countIterator;
+
+    private void Start()
+    {
+        achievementManager = GameObject.Find("AchievementManager");
+        amScript = achievementManager.GetComponent<AchievementManager>();
+    }
+
+    private void Update()
+    {
+        if (GameMainManager.Instance._levelTimePlayed >= (timeToSurvive * 60))
+        {
+            if (!isAwarded)
+            {
+                ShowAchievement();
+            }
+        }       
+    }
+
+    private void ShowAchievement()
+    {
+        GameObject newAchievementVisual = (GameObject)Instantiate(amScript.visualAchievement);
+        newAchievementVisual.transform.SetParent(amScript.canvasUI.transform, false);
+        newAchievementVisual.transform.GetChild(1).GetComponent<Text>().text = this.gameObject.name;
+        newAchievementVisual.transform.localScale = new Vector3(1, 1, 1);
+        isAwarded = true;
+        AwardAchievementToServer();
+    }
+
+    private void AwardAchievementToServer()
+    {
+        Debug.Log("Updating Server");
+        ServerTalk ST = amScript.serverTalkComponent.GetComponent<ServerTalk>();
+        StartCoroutine(ST.UpdatePlayerAchievement(this.name, ST.playerHash));
+    }
 }
--- Assets/Production/Achievements/UI Elements/Scripts/AchievementManager.cs
+++ Assets/Production/Achievements/UI Elements/Scripts/AchievementManager.cs
@@ -107,6 +107,8 @@ public class AchievementManager : MonoBehaviour
         if (achiev.achieveType == achievementType.TIME_SURVIVE)
         {
             achievementLogger.gameObject.AddComponent<AchievementCountSurvival>();
+            AchievementCountSurvival achCountTime = achievementLogger.GetComponent<AchievementCountSurvival>();
+            achCountTime.timeToSurvive = achiev.achieveTimeSurvive;
         }
     }
 }
